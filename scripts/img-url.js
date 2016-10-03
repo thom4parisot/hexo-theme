d83750edbed9208af8143bf8ce8eeb7a35d40d80
Thomas Parisot <thomas.parisot@bbc.co.uk>
2016-10-03 19:59:31 +0100

    Prefix cover URL only in production mode

--- scripts/img-url.js
+++ scripts/img-url.js
@@ -3,11 +3,12 @@
 const { parse } = require('url');
 
 hexo.extend.helper.register('img_url', function(src, post) {
-  const prefix = process.env.NODE_ENV === 'production' ? this.config.url : '';
+  const root = process.env.NODE_ENV === 'production' ? this.config.url : '';
+  const prefix = root.replace(this.config.root.slice(0, -1), '');
 
   return parse(src).protocol
     ? src.replace('_c_d.jpg', '_b_d.jpg')
-    : (post
+    : (post && src[0] !== '/'
       ? prefix + this.url_for(`${post.slug}/${src}`)
       : prefix + this.url_for(src));
 });
