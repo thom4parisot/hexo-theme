b780c9f20a4fcc6d023c12d8bc3995b348f5a0de
Thomas Parisot <thomas.parisot@bbc.co.uk>
2016-10-04 18:31:38 +0100

    Compute title and description from helpers, not partials

---
+++ scripts/meta.js
@@ -0,0 +1,25 @@
+'use strict';
+
+hexo.extend.helper.register('description', function(page) {
+  let description = '';
+
+  if (page.excerpt && page.more) {
+    description = page.excerpt;
+  }
+  else if (page.title && !page.excerpt && page.more) {
+    description = page.content;
+  }
+  else {
+    description = this.config.description;
+  }
+
+  return this.strip_html(description)
+    .trim()
+    .replace(/(\r\n|\n|\r)/gm, "&#10;");
+});
+
+hexo.extend.helper.register('title', function(page) {
+  return page.title
+    ? [page.title, this.config.title].join(" — ")
+    : [this.config.title, this.config.subtitle].join(" — ");
+});
