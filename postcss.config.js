7bfe393eac68831734b5e597f08bd526d0ebce2d
Thomas Parisot <oncletom@users.noreply.github.com>
2018-10-02 22:28:53 +0200

    Expanded header and article card

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-url')([
+      { filter: '**/*.woff2', url: 'inline' },
+    ]),
     require('postcss-preset-env'),
     require('autoprefixer'),
   ]
