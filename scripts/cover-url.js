28987e8a797851931f101ca08d69d243da0f0cf9
Thomas Parisot <oncletom@users.noreply.github.com>
2018-11-26 22:04:30 +0100

    Update coverUrl algo

--- scripts/cover-url.js
+++ scripts/cover-url.js
@@ -3,7 +3,7 @@
 
 hexo.extend.helper.register('coverUrl', function(post, config) {
   const { default_cover, url:siteUrl } = config;
-  var url = post.cover ? (post.cover.url || post.cover) : '';
+  var url = post.cover ? (post.cover.url || post.cover) : (post.image ? '/' + post.image.trimStart('/') : '');
 
   if (!url && default_cover) {
     url = default_cover;
