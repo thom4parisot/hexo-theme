c1a04a4b41c84414de85d9bb091e48e9fe5ed8b2
Thomas Parisot <oncletom@users.noreply.github.com>
2018-12-16 14:31:40 +0100

    fix: image on main page

--- scripts/cover-url.js
+++ scripts/cover-url.js
@@ -3,18 +3,10 @@
 const url_for = hexo.extend.helper.store['url_for'].bind(hexo);
 
 function coverUrl(post, config) {
-  const { default_cover, url:siteUrl='' } = config;
+  const {url:siteUrl=''} = config;
   var url = post.cover ? (post.cover.url || post.cover) : (post.image ? '/' + post.image.trimStart('/') : '');
 
-  if (!url && default_cover) {
-    url = default_cover;
-  }
-
-  if (url){
-    return siteUrl + url_for(url);
-  }
-
-  return url;
+  return url ? (siteUrl + url_for(url)) : '';
 }
 
 hexo.extend.filter.register('before_post_render', function(data){
