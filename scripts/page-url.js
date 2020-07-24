72da02857009377770cd44ce4003702f326c1786
Thomas Parisot <oncletom@users.noreply.github.com>
2020-07-24 14:32:39 +0200

    Upgrade theme to hexo@4, with pretty url configuration

--- scripts/page-url.js
+++ scripts/page-url.js
@@ -1,5 +1,5 @@
 'use strict';
 
 hexo.extend.helper.register('url_for_page', function(path, options) {
-  return this.url_for(path, options).replace(/index.html$/, '');
+  return this.url_for(path, options);
 });
