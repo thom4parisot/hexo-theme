d14e8ea99fbaf04fbdd71d9f2965d8e57b1406cd
Thomas Parisot <oncletom@users.noreply.github.com>
2020-07-24 14:33:06 +0200

    fix: display archives, without journal entries
    they have their own index

--- scripts/archives.js
+++ scripts/archives.js
@@ -4,7 +4,7 @@ const groupBy = require('group-array');
 
 hexo.extend.generator.register('archive_index', function(locals){
   const years = Object.entries(groupBy(
-    locals.posts.sort('date', -1).toArray(),
+    locals.posts.find({ layout: 'post' }).sort('date', -1).toArray(),
     (el) => el.date.year()
   )).sort((a, b) => b[0] - a[0]);
 
